228daa445a5696dbff54765d3fdfb97e92c33d07
Lksman <69352306+Lksman@users.noreply.github.com>
2021-07-23 12:21:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,101 +1,189 @@
 #!/bin/bash
-# Entrypoint for Docker Container
+set -eu
 
+cd /var/www/html
 
-DB_TYPE=${DB_TYPE:-'mysql'}
-DB_HOST=${DB_HOST:-'mysql'}
-DB_PORT=${DB_PORT:-'3306'}
-DB_SOCK=${DB_SOCK:-}
-DB_NAME=${DB_NAME:-'limesurvey'}
-DB_TABLE_PREFIX=${DB_TABLE_PREFIX:-'lime_'}
-DB_USERNAME=${DB_USERNAME:-'limesurvey'}
-DB_PASSWORD=${DB_PASSWORD:-}
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
 
-ADMIN_USER=${DB_ADMIN_USER:-'admin'}
-ADMIN_NAME=${DB_ADMIN_NAME:-'admin'}
-ADMIN_EMAIL=${DB_ADMIN_EMAIL:-'-'}
-ADMIN_PASSWORD=${DB_ADMIN_PASSWORD:-'-'}
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	file_env 'LIMESURVEY_DB_HOST' 'mysql'
+	file_env 'LIMESURVEY_TABLE_PREFIX' ''
+    file_env 'LIMESURVEY_ADMIN_NAME' 'Lime Administrator'
+    file_env 'LIMESURVEY_ADMIN_EMAIL' 'lime@lime.lime'
+    file_env 'LIMESURVEY_ADMIN_USER' ''
+    file_env 'LIMESURVEY_ADMIN_PASSWORD' ''
+    file_env 'LIMESURVEY_DEBUG' '0'
+    file_env 'LIMESURVEY_SQL_DEBUG' '0'
+    file_env 'MYSQL_SSL_CA' ''
+    file_env 'LIMESURVEY_USE_INNODB' ''
 
-PUBLIC_URL=${PUBLIC_URL:-}
-URL_FORMAT=${URL_FORMAT:-'path'}
+	# if we're linked to MySQL and thus have credentials already, let's use them
+	file_env 'LIMESURVEY_DB_USER' "${MYSQL_ENV_MYSQL_USER:-root}"
+	if [ "$LIMESURVEY_DB_USER" = 'root' ]; then
+		file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-}"
+	else
+		file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_PASSWORD:-}"
+	fi
+	file_env 'LIMESURVEY_DB_NAME' "${MYSQL_ENV_MYSQL_DATABASE:-limesurvey}"
+	if [ -z "$LIMESURVEY_DB_PASSWORD" ]; then
+		echo >&2 'error: missing required LIMESURVEY_DB_PASSWORD environment variable'
+		echo >&2 '  Did you forget to -e LIMESURVEY_DB_PASSWORD=... ?'
+		echo >&2
+		echo >&2 '  (Also of interest might be LIMESURVEY_DB_USER and LIMESURVEY_DB_NAME.)'
+		exit 1
+	fi
 
+    echo >&2 "Copying default container default config files into config volume..."
+    cp -dR /var/lime/application/config/* application/config
 
-# Check if database is available
-if [ -z "$DB_SOCK" ]; then
-    until nc -z -v -w30 $DB_HOST $DB_PORT
-    do
-        echo "Info: Waiting for database connection..."
-        sleep 5
-    done
-fi
-
-
-# Check if already provisioned
-if [ -f application/config/config.php ]; then
-    echo 'Info: config.php already provisioned'
-else
-    echo 'Info: Generating config.php'
-
-    if [ "$DB_TYPE" = 'mysql' ]; then
-        echo 'Info: Using MySQL configuration'
-        DB_CHARSET=${DB_CHARSET:-'utf8mb4'}
-        cp application/config/config-sample-mysql.php application/config/config.php
+    if ! [ -e plugins/index.html ]; then
+        echo >&2 "No index.html file in plugins dir in $(pwd) Copying defaults..."
+        cp -dR /var/lime/plugins/* plugins
     fi
 
-    if [ "$DB_TYPE" = 'pgsql' ]; then
-        echo 'Info: Using PostgreSQL configuration'
-        DB_CHARSET=${DB_CHARSET:-'utf8'}
-        cp application/config/config-sample-pgsql.php application/config/config.php
+    if ! [ -e upload/index.html ]; then
+        echo >&2 "No index.html file upload dir in $(pwd) Copying defaults..."
+        cp -dR /var/lime/upload/* upload
     fi
 
-    # Set Database config
-    if [ ! -z "$DB_SOCK" ]; then
-        echo 'Info: Using unix socket'
-        sed -i "s#\('connectionString' => \).*,\$#\\1'${DB_TYPE}:unix_socket=${DB_SOCK};dbname=${DB_NAME};',#g" application/config/config.php
-    else
-        echo 'Info: Using TCP connection'
-        sed -i "s#\('connectionString' => \).*,\$#\\1'${DB_TYPE}:host=${DB_HOST};port=${DB_PORT};dbname=${DB_NAME};',#g" application/config/config.php
+    if ! [ -e application/config/config.php ]; then
+        echo >&2 "No config file in $(pwd) Copying default config file..."
+		#Copy default config file but also allow for the addition of attributes
+awk '/lime_/ && c == 0 { c = 1; system("cat") } { print }' application/config/config-sample-mysql.php > application/config/config.php <<'EOPHP'
+'attributes' => array(),
+EOPHP
     fi
 
-    sed -i "s#\('username' => \).*,\$#\\1'${DB_USERNAME}',#g" application/config/config.php
-    sed -i "s#\('password' => \).*,\$#\\1'${DB_PASSWORD}',#g" application/config/config.php
-    sed -i "s#\('charset' => \).*,\$#\\1'${DB_CHARSET}',#g" application/config/config.php
-    sed -i "s#\('tablePrefix' => \).*,\$#\\1'${DB_TABLE_PREFIX}',#g" application/config/config.php
-
-    # Set URL config
-    sed -i "s#\('urlFormat' => \).*,\$#\\1'${URL_FORMAT}',#g" application/config/config.php
-
-    # Set Public URL
-    if [ -z "$PUBLIC_URL" ]; then
-        echo 'Info: Setting PublicURL'
-        sed -i "s#\('debug'=>0,\)\$#'publicurl'=>'${PUBLIC_URL}',\n\t\t\\1 #g" application/config/config.php
-    fi
-fi
-
-
-# Check if LimeSurvey database is provisioned
-echo 'Info: Check if database already provisioned. Nevermind the Stack trace.'
-php application/commands/console.php updatedb
-
-
-if [ $? -eq 0 ]; then
-    echo 'Info: Database already provisioned'
-else
-    # Check if DB_PASSWORD is set
-    if [ -z "$DB_PASSWORD" ]; then
-        echo >&2 'Error: Missing DB_PASSWORD'
-        exit 1
+	# Install BaltimoreCyberTrustRoot.crt.pem
+	if ! [ -e BaltimoreCyberTrustRoot.crt.pem ]; then
+		echo "Downloading BaltimoreCyberTrustroot.crt.pem"
+		if curl -o BaltimoreCyberTrustRoot.crt.pem -fsL "https://www.digicert.com/CACerts/BaltimoreCyberTrustRoot.crt.pem"; then
+            echo "Downloaded successfully"
+        else
+            echo "Failed to download certificate - continuing anyway"
+        fi
     fi
 
-    # Check if DB_PASSWORD is set
-    if [ -z "$DB_ADMIN_PASSWORD" ]; then
-        echo >&2 'Error: Missing ADMIN_PASSWORD'
-        exit 1
+    # see http://stackoverflow.com/a/2705678/433558
+    sed_escape_lhs() {
+        echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+    }
+    sed_escape_rhs() {
+        echo "$@" | sed -e 's/[\/&]/\\&/g'
+    }
+    php_escape() {
+        php -r 'var_export(('$2') $argv[1]);' -- "$1"
+    }
+    set_config() {
+        key="$1"
+        value="$2"
+        sed -i "/'$key'/s>\(.*\)>$value,1"  application/config/config.php
+    }
+
+    set_config 'connectionString' "'mysql:host=$LIMESURVEY_DB_HOST;port=3306;dbname=$LIMESURVEY_DB_NAME;'"
+    set_config 'tablePrefix' "'$LIMESURVEY_TABLE_PREFIX'"
+    set_config 'username' "'$LIMESURVEY_DB_USER'"
+    set_config 'password' "'$LIMESURVEY_DB_PASSWORD'"
+	set_config 'urlFormat' "'path'"
+    set_config 'debug' "$LIMESURVEY_DEBUG"
+    set_config 'debugsql' "$LIMESURVEY_SQL_DEBUG"
+
+	if [ -n "$MYSQL_SSL_CA" ]; then
+		set_config 'attributes' "array(PDO::MYSQL_ATTR_SSL_CA => '\/var\/www\/html\/$MYSQL_SSL_CA', PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false)"
     fi
 
-    echo ''
-    echo 'Running console.php install'
-    php application/commands/console.php install $DB_ADMIN_USER $DB_ADMIN_PASSWORD $DB_ADMIN_NAME $DB_ADMIN_EMAIL
+	if [ -n "$LIMESURVEY_USE_INNODB" ]; then
+		#If you want to use INNODB - remove MyISAM specification from LimeSurvey code
+		sed -i "/ENGINE=MyISAM/s/\(ENGINE=MyISAM \)//1" application/core/db/MysqlSchema.php
+        #Also set mysqlEngine in config file
+		sed -i "/\/\/ Update default LimeSurvey config here/s//'mysqlEngine'=>'InnoDB',/" application/config/config.php
+		DBENGINE='InnoDB'
+    fi
+
+
+    chown www-data:www-data -R tmp 
+    chown www-data:www-data -R plugins
+    mkdir -p upload/surveys
+    chown www-data:www-data -R upload 
+    chown www-data:www-data -R application/config
+
+	DBSTATUS=$(TERM=dumb php -- "$LIMESURVEY_DB_HOST" "$LIMESURVEY_DB_USER" "$LIMESURVEY_DB_PASSWORD" "$LIMESURVEY_DB_NAME" "$LIMESURVEY_TABLE_PREFIX" "$MYSQL_SSL_CA" <<'EOPHP'
+<?php
+// database might not exist, so let's try creating it (just to be safe)
+error_reporting(E_ERROR | E_PARSE);
+$stderr = fopen('php://stderr', 'w');
+list($host, $socket) = explode(':', $argv[1], 2);
+$port = 0;
+if (is_numeric($socket)) {
+        $port = (int) $socket;
+        $socket = null;
+}
+$maxTries = 10;
+do {
+    $con = mysqli_init();
+    if (isset($argv[6]) && !empty($argv[6])) {
+	    mysqli_ssl_set($con,NULL,NULL,"/var/www/html/" . $argv[6],NULL,NULL);
+    }
+    $mysql = mysqli_real_connect($con,$host, $argv[2], $argv[3], '', $port, $socket, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+        if (!$mysql) {
+                fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+                --$maxTries;
+                if ($maxTries <= 0) {
+                        exit(1);
+                }
+                sleep(3);
+        }
+} while (!$mysql);
+if (!$con->query('CREATE DATABASE IF NOT EXISTS `' . $con->real_escape_string($argv[4]) . '`')) {
+        fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $con->error . "\n");
+        $con->close();
+        exit(1);
+}
+$con->select_db($con->real_escape_string($argv[4]));
+$inst = $con->query("SELECT * FROM `" . $con->real_escape_string($argv[5]) . "users" . "`");
+$con->close();
+if ($inst->num_rows > 0) {
+        exit("DBEXISTS");
+} else {
+        exit(0);
+}
+EOPHP
+)
+
+
+	if [ "$DBSTATUS" != "DBEXISTS" ] &&  [ -n "$LIMESURVEY_ADMIN_USER" ] && [ -n "$LIMESURVEY_ADMIN_PASSWORD" ]; then
+        echo >&2 'Database not yet populated - installing Limesurvey database'
+	    php application/commands/console.php install "$LIMESURVEY_ADMIN_USER" "$LIMESURVEY_ADMIN_PASSWORD" "$LIMESURVEY_ADMIN_NAME" "$LIMESURVEY_ADMIN_EMAIL" verbose
+	fi
+
+	if [ -n "$LIMESURVEY_ADMIN_USER" ] && [ -n "$LIMESURVEY_ADMIN_PASSWORD" ]; then
+		echo >&2 'Updating password for admin user'
+        php application/commands/console.php resetpassword "$LIMESURVEY_ADMIN_USER" "$LIMESURVEY_ADMIN_PASSWORD"
+	fi
+
+    #flush asssets (clear cache on restart)
+    php application/commands/console.php flushassets
+
 fi
 
 exec "$@"
